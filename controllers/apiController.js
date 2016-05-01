3cb00a75bb40b272d4c02e0d0de3bc1ea3846074
HugoMatilla <hugomatilla@gmail.com>
2016-05-01 13:34:27 +0200

    Add api method /persons

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,3 +1,6 @@
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
 module.exports = function(app) {
 	
 	app.get('/api/person/:id', function(req, res) {
@@ -5,7 +8,13 @@ module.exports = function(app) {
 		res.json({ firstname: 'John', lastname: 'Doe' });
 	});
 	
-	app.post('/api/person', function(req, res) {
+	app.post('/api/person',jsonParser, function(req, res) {
+		res.send(`Thank you for the JSON data ${req.body.firstname} ${req.body.lastname}`);
+        console.log(req.body.firstname);
+        console.log(req.body.lastname);	
+	});
+	
+	app.post('/api/persons', function(req, res) {
 		res.writeHead(404);
 	    res.end();
 	});
