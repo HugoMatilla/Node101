8751d2f0bd939bb7e632d7d4b7143e11fa3180ee
HugoMatilla <hugomatilla@gmail.com>
2016-05-02 17:39:45 +0200

    Add Request

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
 	next();
 });
-
+var request = require('request');
+request('http://starwars.wikia.com/api/v1/Articles/Top?expand=1', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+})
 htmlController(app);
 
 apiController(app);
